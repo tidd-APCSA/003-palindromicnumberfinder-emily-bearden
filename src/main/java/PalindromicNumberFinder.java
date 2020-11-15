3228eb14318d405009659c52678302a59a9a68d4
Emily Bearden <emily.bearden@yahoo.com>
2020-11-15 01:42:58 +0000

    finished program

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -4,6 +4,7 @@ public class PalindromicNumberFinder {
     // constructor
     public PalindromicNumberFinder(int num){
         this.num = num;
+        num += 1;
     }
 
     // accessor/getter method
@@ -13,19 +14,66 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+    // System.out.println(reverseNum(num));
+     String mun = reverseNum(num);
+     Integer mun1 = Integer.valueOf(mun);
+     while (testPalindromicNum(mun1) == false){
+        mun1++;
+     }
+     //System.out.println("palindrom - original: " + (mun1 - num));
+     return mun1 - num;
+    //  if (testPalindromicNum(mun1) == true){
+    //    return num;
+    //  }else {
+    //    num++;
+    //  }
+    //   return num;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
-
+      String number = String.valueOf(num);
+      boolean pal = true;
+      int length = number.length();
+      // System.out.println("length: " + length);
+      // System.out.println("start: " + number.substring(1-1, 1));
+      // System.out.println("end: " + number.substring(length - 1, length));
+      String start = number.substring(1-1, 1);
+      String end = number.substring(length - 1, length);
+      //System.out.println(number.substring(0, 0+1));
+      //System.out.println(number.substring(length-0, length));
+     for (int i = 1; i < 2; i++){
+       String start1 = number.substring(i-1, i);
+       Integer start2 = Integer.valueOf(start);
+       String end1 = number.substring(length-i, length);
+       Integer end2 = Integer.valueOf(end);
+        if (start2 == end2){
+         start1 = number.substring(i, i+1);
+         start2 = Integer.valueOf(start1);
+         end1 = number.substring(length - i - 1, length - i);
+         end2 = Integer.valueOf(end1);
+          //pal = true;
+         // System.out.println("i: " + i);
+        } else {
+          return false;
+        }
+      }
+      return pal;
     }
 
 
-    // this is a helper method for testPalindromicNum. It should reverse the number and return it.
+    // // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-
+    num += 1;
+    String number = String.valueOf(num);
+    String reverse = "";
+    for (int i = number.length(); i > 0; i--){
+      String start = number.substring(i-1,i);
+      reverse += start;
+    }
+    Integer num1 = Integer.valueOf(reverse);
+   // System.out.println(testPalindromicNum(num1));
+      return reverse;
 
 
     }
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,12 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-
+      //int num1 = 123;
+      Integer num2 = 72927;
+      PalindromicNumberFinder test = new PalindromicNumberFinder(num2);
+      //System.out.println("number is " + test.getNum());
+      test.searchForPalindromicNum(num2);
+      System.out.println(test);
+      //System.out.println(test.searchForPalindromicNum(num2));
     }
 }
