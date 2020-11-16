9f139d6da033489e885cfa2dd77af785a24b172a
Emily Bearden <emily.bearden@yahoo.com>
2020-11-16 15:29:09 +0000

    maybe its fixed now?

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -3,11 +3,11 @@ public class PalindromicRunner {
         // be sure you're testing each method as you work then produce your final output using the toString method
       //int num1 = 123;
       Integer num2 = 72927;
-      PalindromicNumberFinder test = new PalindromicNumberFinder(num2);
+      PalindromicNumberFinder yeet = new PalindromicNumberFinder(num2);
      // System.out.println("reverse: " + test.reverseNum(num2));
       //System.out.println("number is " + test.getNum());
-      test.searchForPalindromicNum(num2);
-      System.out.println(test);
+      yeet.searchForPalindromicNum(num2);
+      System.out.println(yeet);
       //System.out.println(test.searchForPalindromicNum(num2));
     }
 }
